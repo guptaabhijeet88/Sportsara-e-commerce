38051b92da6fd7acdb520e93cab0a9b498dc9fd5
guptaabhijeet88 <abhijeetgupta9702@gmail.com>
2026-04-24 21:43:11 +0530

    Fix frontend folder tracking for Vercel

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,2 +1,2 @@
-const API = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API = process.env.REACT_APP_API_URL || 'https://sportsara-e-commerce.onrender.com/api';
 export default API;
